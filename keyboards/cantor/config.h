50dc0dbf2caee090fa7136bc46239c16aa167886
Klesh Wong <klesh@qq.com>
2022-10-18 02:09:05 +0800

    feat: cantor-remix 36keys

--- keyboards/cantor/config.h
+++ keyboards/cantor/config.h
@@ -7,6 +7,14 @@
 #define SERIAL_USART_TX_PIN B6     // USART TX pin
 #define SERIAL_USART_RX_PIN B7     // USART RX pin
 
+/* Top left key on left half */
+#define BOOTMAGIC_LITE_ROW    0
+#define BOOTMAGIC_LITE_COLUMN 1
+/* Top right key on right half */
+#define BOOTMAGIC_LITE_ROW_RIGHT    4
+#define BOOTMAGIC_LITE_COLUMN_RIGHT 4
+
+
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
@@ -22,3 +30,11 @@
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
+
+
+#define MASTER_RIGHT
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PS2_CLOCK_PIN A9
+#define PS2_DATA_PIN  B2
+#define PS2_MOUSE_ROTATE 270
+
--- keyboards/cantor/keymaps/vial/keymap.c
+++ keyboards/cantor/keymaps/vial/keymap.c
@@ -4,44 +4,10 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-     /*
-      * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-      * │Tab│ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │Bsp│
-      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │Ctl│ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │ ' │
-      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │Sft│ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │Sft│
-      * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
-      *               ┌───┐                   ┌───┐
-      *               │GUI├───┐           ┌───┤Alt│
-      *               └───┤   ├───┐   ┌───┤   ├───┘
-      *                   └───┤Bsp│   │Ent├───┘
-      *                       └───┘   └───┘
-      */
-    [0] = LAYOUT_split_3x6_3(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            KC_LGUI, KC_SPC, KC_BSPC,          KC_ENT, KC_SPC, KC_RALT
-    ),
+	[0] = LAYOUT_split_3x6_3(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_LCTL, LALT_T(KC_A), LSFT_T(KC_S), LGUI_T(KC_D), LCTL_T(KC_F), KC_G, LT(3,KC_H), RCTL_T(KC_J), RGUI_T(KC_K), RSFT_T(KC_L), RALT_T(KC_SCLN), KC_QUOT, KC_LSFT, LCTL_T(KC_Z), KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, RCTL_T(KC_SLSH), KC_RSFT, LALT_T(KC_TAB), LT(1,KC_ENT), LT(3,KC_ESC), KC_BTN1, LSFT_T(KC_SPC), LT(2,KC_BSPC)),
+	[1] = LAYOUT_split_3x6_3(KC_NO, KC_F9, KC_F10, KC_F11, KC_F12, LGUI(KC_F3), KC_DOT, KC_7, KC_8, KC_9, KC_EQL, KC_NO, KC_NO, KC_F5, KC_F6, KC_F7, KC_F8, LGUI(KC_F2), KC_MINS, KC_4, KC_5, KC_6, KC_PLUS, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, LGUI(KC_F1), KC_ASTR, KC_1, KC_2, KC_3, KC_SLSH, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_0, KC_DOT, KC_TRNS),
+	[2] = LAYOUT_split_3x6_3(KC_NO, KC_QUOT, KC_DQUO, KC_EXLM, KC_ASTR, KC_TILD, KC_NO, KC_UNDS, KC_NO, KC_GRV, KC_PIPE, KC_NO, KC_NO, KC_AT, KC_HASH, KC_DLR, KC_LPRN, KC_RPRN, TO(4), KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_NO, KC_NO, KC_NO, KC_PERC, KC_NO, KC_CIRC, KC_BSLS, KC_AMPR, KC_NO, KC_NO, KC_EQL, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+	[3] = LAYOUT_split_3x6_3(KC_NO, LALT(KC_F4), TO(4), KC_END, KC_BTN3, KC_BTN2, KC_WBAK, LCTL(KC_PGUP), LCTL(KC_PGDN), KC_WFWD, KC_MPRV, KC_NO, KC_NO, KC_HOME, KC_BSPC, KC_DEL, LCTL(KC_RGHT), KC_BTN1, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, KC_NO, LGUI(KC_F1), LGUI(KC_F2), RCS(KC_C), RCS(KC_V), LCTL(KC_LEFT), KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN3, KC_BTN2, KC_TRNS)
 
-    [1] = LAYOUT_split_3x6_3(
-        KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-                                            KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS
-    ),
-    [2] = LAYOUT_split_3x6_3(
-        KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-                                            KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS
-    ),
-    [3] = LAYOUT_split_3x6_3(
-        KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-                                            KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS
-    )
 };
 

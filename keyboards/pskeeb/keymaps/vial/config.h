7888f2857eb1925cbb1d9af07757f7b45a9c36e5
Klesh Wong <klesh@qq.com>
2022-10-19 23:35:29 +0800

    [Keyboard] Add pskeeb support

---
+++ keyboards/pskeeb/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+
+/* keyboard uid */
+#define VIAL_KEYBOARD_UID {0xE7, 0xFD, 0x85, 0xFB, 0xCB, 0x49, 0x8B, 0x20}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 4 }
+
+/* default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4

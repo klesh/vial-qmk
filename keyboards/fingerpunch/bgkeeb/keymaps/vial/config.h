2416f75a8a2ff10a86c8a5a9a30278f0c012a7c0
Klesh Wong <klesh@qq.com>
2022-12-21 20:51:28 +0800

    [Keyboard] Add bgkeeb

---
+++ keyboards/fingerpunch/bgkeeb/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+
+/* keyboard uid */
+#define VIAL_KEYBOARD_UID {0xB9, 0x5D, 0xFC, 0x90, 0xE2, 0x71, 0x7C, 0x6E}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 4 }
+
+/* default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+
+#define VIAL_KEY_OVERRIDE_ENTRIES 4

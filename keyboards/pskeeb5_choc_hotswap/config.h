613cffc764db4c15f07b864b12e5e7b9177ffeb7
Klesh Wong <klesh@qq.com>
2026-01-04 10:47:53 +0800

    feat: added pskeeb5 keyboard

---
+++ keyboards/pskeeb5_choc_hotswap/config.h
@@ -0,0 +1,13 @@
+// Copyright 2024 Klesh Wong (@klesh)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x94, 0xEC, 0x62, 0x87, 0x24, 0x5F, 0xE2, 0xB1}
+#define MASTER_RIGHT
+
+/* keyboard uid */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+// #define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+// #define VIAL_UNLOCK_COMBO_COLS { 4, 4 }

8265f31532355eefa93365307c89378d82c04cda
Klesh Wong <klesh@qq.com>
2026-08-31 11:48:11 +0800

    fix: merge conflict

--- keyboards/pskeeb3_cherry_hotswap/rules.mk
+++ keyboards/pskeeb3_cherry_hotswap/rules.mk
@@ -1,9 +1,5 @@
 SERIAL_DRIVER = vendor
 SPLIT_KEYBOARD = yes
-<<<<<<< HEAD
 ENCODER_ENABLE = yes
 # RGB_MATRIX_ENABLE = yes
 PS2_ENABLE = yes
-=======
-ENCODER_ENABLE = yes
->>>>>>> a0b7139b8a (fix: pskeeb2 cherry hotswap working)
